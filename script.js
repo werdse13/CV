ddebd2284cd2f89436d411145cb4bab2fe9d044e
pmc <werdse13@gmail.com>
2026-04-26 22:34:54 +0100

    Improved resposiveness and proportions on all sized sites.

--- script.js
+++ script.js
@@ -1,25 +1,7 @@
-/*window.onscroll = function () { scrollFunction(); };
-
-function scrollFunction() {
-  const scrolled = document.body.scrollTop > 10 || document.documentElement.scrollTop > 10;
-
-  document.querySelector("header").style.fontSize = scrolled ? "50%" : "100%";
-  document.querySelector(".self_img").style.height = scrolled ? "100px" : "140px";
-
-  document.querySelectorAll(".header_p").forEach(el => {
-    el.style.fontSize = scrolled ? "10px" : "15px";
-  });
-
-  document.querySelectorAll(".linked_img").forEach(el => {
-    el.style.height = scrolled ? "20px" : "40px";
-  });
-}
-  */
-
 window.onscroll = function () { scrollFunction(); };
 
 function scrollFunction() {
-  const scrolled = document.body.scrollTop > 5 || document.documentElement.scrollTop > 5;
+  const scrolled = document.body.scrollTop > 100 || document.documentElement.scrollTop > 100;
 
   document.querySelector("header").classList.toggle("scrolled", scrolled);
 }
