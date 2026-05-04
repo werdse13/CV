cb5d73654750d05aa7cd74d265112e982adf7bea
pmc <werdse13@gmail.com>
2026-05-04 16:41:05 +0100

    Added additional JS for scrolling to top and form submition validation. Added text to all areas. created a scroll to top link in my name and picture. created a form in contact box. fixed sidebar being put below footer when in mobile and tablet size. Improved responsiveness.

--- script.js
+++ script.js
@@ -5,3 +5,32 @@ function scrollFunction() {
 
   document.querySelector("header").classList.toggle("scrolled", scrolled);
 }
+
+function submitForm() {
+  if (document.getElementById("name").value == "") {
+    alert("Name must be filled out");
+    return false;
+  }
+  if (document.getElementById("email").value == "") {
+    alert("Email must be filled out");
+    return false;
+  }
+  if (document.getElementById("subject").value == "") {
+    alert("Subject must be filled out");
+    return false;
+  }
+  if (document.getElementById("message").value == "") {
+    alert("Message must be filled out");
+    return false;
+  }
+  alert("Message sent!");
+  document.getElementById("contactForm").reset();
+}
+
+function scrollToTop(e) {
+  e.preventDefault();
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+}
